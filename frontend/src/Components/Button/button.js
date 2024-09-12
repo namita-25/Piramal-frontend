731e962519f42935b56cfacb446be6e8026e6fcd
Namita g <namita_g@tekditechnologies.com>
2024-09-12 12:47:11 +0530

    add folder structure

---
+++ frontend/src/Components/Button/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "@mui/material/Button";
+function button() {
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
+}
+
+export default button;
